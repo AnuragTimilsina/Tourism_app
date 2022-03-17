07fe34df340084570e3056ddb7ccf51fa75a9ad5
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-03-17 21:57:32 +0545

    tourist and agency models made.

--- tourism_app/agencies/models.py
+++ tourism_app/agencies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class agency(AbstractUser):
+    name = models.CharField(max_length=300, blank=False, default="")
+
+    # Contacts
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number should be in correct format'. Up to 15 digits allowed.")
+
+    Landline_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True)
+    mobile_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True)
+
+    address = models.CharField(max_length=300, blank=True)
+    agency_profile_pic = models.ImageField(default="")
+
+    def __str__(self):
+        return self.name
+
