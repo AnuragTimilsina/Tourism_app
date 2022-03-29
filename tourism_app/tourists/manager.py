3d43203d5b5c466133b40b5d30150a9910b4978f
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-03-29 10:40:05 +0545

    managers modified

--- tourism_app/tourists/manager.py
+++ tourism_app/tourists/manager.py
@@ -7,14 +7,23 @@ class CustomUserManager(BaseUserManager):
         Create and save a User with the given email and password.
     """
 
-    def create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_('The Email must be set'))
+        if not password:
+            raise ValueError(_('Password is not provided'))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db) 
         return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         """
@@ -29,5 +38,5 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_suoeruser=True!'))
 
-        return self.create_user(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
