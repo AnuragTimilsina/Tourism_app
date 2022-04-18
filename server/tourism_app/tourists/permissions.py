a62c7b36b59b95c1f5344d25e9731756da734d7e
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-04-18 07:45:08 +0545

    Agency and tourist permissions made

---
+++ server/tourism_app/tourists/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class agencyPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_agency == True
+
+
+class touristPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_tourist == True
