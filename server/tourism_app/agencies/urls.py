32a4838b38f2d4e75c15573764f1197f8dddf48f
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-04-23 08:10:20 +0545

    Done with agency tourist verification

--- server/tourism_app/agencies/urls.py
+++ server/tourism_app/agencies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AgencyAuthToken
+from .views import AgencyAuthToken, VerifyAgencyToken
 
 urlpatterns = [
     path('api/authenticate/', AgencyAuthToken.as_view(), name='agency-login'),
+    path('api/verifyagencytoken/', VerifyAgencyToken.as_view(), name='verify-agency-token'),
 ]
--- server/tourism_app/agencies/views.py
+++ server/tourism_app/agencies/views.py
@@ -17,3 +17,28 @@ class AgencyAuthToken(ObtainAuthToken):
             return Response({'token': token.key, 'id': token.user_id})
         else:
             return Response({'Error': "Agency not registered!!!"})
+
+
+# Verify Agency Token: 
+from rest_framework.views import APIView
+
+
+class VerifyAgencyToken(APIView):
+    '''
+        This is the endpoint to verify token. 
+    '''
+
+    def post(self, request, *args, **kwargs):
+        user = [int(user) for user in User.objects.filter(is_agency=True).values_list('id', flat=True)]
+        tokens = []
+        for id in user:
+            tokens.append([str(tok) for tok in Token.objects.filter(user=id)])
+        
+        token = [tok[0] for tok in tokens if bool(tok)==True]
+        
+        # print(tokens)
+        if request.data['token'] in token:
+            return Response({'token_verified':True})
+        return Response({'token_verified':False})
+
+
