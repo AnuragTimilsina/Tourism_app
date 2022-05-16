12976e53170ba92c8f74f2053908703052d70085
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-05-16 08:28:47 +0545

    Changed allowed host in production

--- server/tourism_app/tourism_app/settings.py
+++ server/tourism_app/tourism_app/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://tourism-app-api.azurewebsites.net/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
